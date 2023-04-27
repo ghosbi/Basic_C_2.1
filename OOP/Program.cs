13b31f86fe245a811187781b2402205612ef4520
Pavel Konstantinov <Pavel.romon@gmail.com>
2023-04-27 22:35:16 +0500

    Закончили задание 6.5.2

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -1,14 +1,39 @@
-﻿namespace FirstApplication.ConsoleApp // Объявление пространства имен для данного кода
+﻿using System; // Подключенное пространство имен
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace FirstApplication.ConsoleApp // Объявление пространства имен для данного кода
 {
     class Program // Объявление класса Program
     {
         static void Main(string[] args) // Объявление метода Main
         {
+            
+        }
+
+        static Department GetCurrentDepartment()
+        {
 
         }
+
+        static void tasksubTask()
+        {
+            var department = GetCurrentDepartment();
+
+            if (department?.Company?.Type == "Банк" && department?.City?.Name == "Санкт-Петербург")
+            {
+                Console.WriteLine("У банка {0} есть отделение в Сантк-Петербурге", department?.Company?.Name ?? "Неизвестная компания");
+            }
+        }
+
+       
     }
+
+    
 }
-        class Pen
+
+class Pen
 {
     public string color;
     public int cost;
@@ -28,6 +53,7 @@
         cost = penCost;
     }
 }
+
 class Rectangle
 {
     public int a;
@@ -54,5 +80,86 @@ class Rectangle
     {
         a = x;
         b = x;
+    }
+}
+
+class Company
+{
+    public string Type;
+    public string Name;
+}
+
+class Department
+{
+    public Company Company;
+    public City City;
+}
+
+class City
+{
+    public string Name;
+}
+
+class bus
+{
+    public int? Load;
+
+    public void PrintStatus()
+    {
+        if (Load.HasValue)
+        {
+            Console.WriteLine("В автобусе {0} пассажиров", Load.Value);
+        }
+        else
+        {
+            Console.WriteLine("Автобус пуст!");
+        }
+    }
+}
+
+class Triangle
+{
+    public int a;
+    public int b;
+    public int c;
+
+    public double Square()
+    {
+
+    }
+
+    public double Perimeter()
+    {
+
+    }
+}
+
+class Square
+{
+    public int side;
+
+    public double Square()
+    {
+
+    }
+
+    public double Perimeter()
+    {
+
+    }
+}
+
+class Circle
+{
+    public double radius;
+
+    public double Square()
+    {
+
+    }
+
+    public double Length()
+    {
+
     }
 }
