033fd260c9fcd822404c6cf141d0bf7f8d30f154
Pavel Konstantinov <Pavel.romon@gmail.com>
2023-06-09 16:54:06 +0500

    9.1.4

---
+++ Exception/Program.cs
@@ -0,0 +1,25 @@
+﻿using System; // Подключенное пространство имен
+using System.Data.Common;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using System.Transactions;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace FirstApplication.ConsoleApp // Объявление пространства имен для данного кода
+{
+    class Program // Объявление класса Program
+    {
+        static void Main(string[] args)
+        {
+            Exception exception = new Exception("Собственное исключение");
+
+            exception.Data.Add("Дата создания:", DateTime.Now);
+
+
+
+            exception.HelpLink = "www.yandex.ru";
+        }
+    }
+}
