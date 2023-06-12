e28a6b4d59a24c4ac9ead98c4fd49dcb0cce247a
Pavel Konstantinov <Pavel.romon@gmail.com>
2023-06-12 22:29:44 +0500

    9.3.7

--- Exception/Program.cs
+++ Exception/Program.cs
@@ -11,12 +11,43 @@ namespace FirstApplication.ConsoleApp // Объявление пространс
 {
     class Program // Объявление класса Program
     {
+        delegate void ShowMessageDelegate();
         public delegate int MinusDelegate(int a, int b);
+        delegate int SumDelegate(int a, int b, int c);
+        delegate bool CheckLengthDelegate(String row);
         static void Main(string[] args)
         {
-            
+            ShowMessageDelegate showMessageDelegate = ShowMessage;
+            showMessageDelegate.Invoke();
+
+            SumDelegate sumDelegate = Sum;
+            int result = sumDelegate.Invoke(1, 30, 120);
+            Console.WriteLine(result);
+
+            CheckLengthDelegate checkLengthDelegate = CheckLength;
+            bool status = checkLengthDelegate.Invoke("Skill_Factory");
+            Console.WriteLine(status);
         }
 
+        static bool CheckLength(string _row)
+        {
+            if (_row.Length > 3)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static int Sum(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+        static void ShowMessage()
+        {
+            Console.WriteLine("Hello World");
+        }
         static int Minus(int a, int b)
         {
             return b - a;
