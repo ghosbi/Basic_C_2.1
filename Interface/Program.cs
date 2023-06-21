d6ee7d5baa02e350dfadc802e872de3972321fb9
Pavel Konstantinov <Pavel.romon@gmail.com>
2023-06-21 16:49:59 +0500

    Complete 10.3.2

--- Interface/Program.cs
+++ Interface/Program.cs
@@ -28,6 +28,38 @@ namespace FirstApplication.ConsoleApp // Объявление пространс
         }
 
     }
+    public class Entity : ICreatable, IDeletable, IUpdatable
+    {
+        public void Create()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface ICreatable
+    {
+        void Create();
+    }
+
+    public interface IDeletable
+    {
+        void Delete();
+    }
+
+    public interface IUpdatable
+    {
+        void Update();
+    }
 
     public class FileManager : IWriter1, IReader, IMailer
     {
