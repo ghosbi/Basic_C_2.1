a6fcee192292379d858324045f19be0e1fee844c
Pavel Konstantinov <Pavel.romon@gmail.com>
2023-07-04 20:30:36 +0500

    Выполнили задание 12.1.2

---
+++ Algorithm12/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Algorithm12
+{
+    class Program // Объявление класса Program
+    {
+        public static async Task Main()
+        {
+            Console.WriteLine("Как вас зовут?");
+            string name = Console.ReadLine();
+            string greetings = "Привет" + name;
+            Console.WriteLine(greetings);
+        }
+    }
+}
