36c0f77bf3944550121de50d8bd421517676dff9
Pavel Konstantinov <Pavel.romon@gmail.com>
2023-07-04 20:35:59 +0500

    Выполнили 12.1.3

--- Algorithm12/Program.cs
+++ Algorithm12/Program.cs
@@ -4,9 +4,24 @@
     {
         public static async Task Main()
         {
+            Console.WriteLine("Сколько элементов будет в массиве?");
+
+            var count = Int32.Parse(Console.ReadLine());
+            var array = new int[count];
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine($"Введите все {count} элемнтов");
+                array[i] = Int32.Parse(Console.ReadLine());
+
+            }
+            Console.WriteLine("Все элементы записаны");
+        }
+        
+        static void Task12_1_2()
+        {
             Console.WriteLine("Как вас зовут?");
             string name = Console.ReadLine();
-            string greetings = "Привет" + name;
+            string greetings = "Привет, " + name;
             Console.WriteLine(greetings);
         }
     }
