021980bdd43356e06a7b23a3f5871c3c8f4e4967
Pavel Konstantinov <Pavel.romon@gmail.com>
2023-07-06 17:54:34 +0500

    Выполнили задание 12.1.5

--- Algorithm12/Program.cs
+++ Algorithm12/Program.cs
@@ -1,8 +1,77 @@
-﻿namespace Algorithm12
+﻿using static Algorithm12.Program;
+
+namespace Algorithm12
 {
     class Program // Объявление класса Program
     {
-        public static async Task Main()
+        public delegate bool EligibleToShowAds(User UserToShowAds);
+        static void Main(string[] args)
+        {
+            User user1 = new User()
+            {
+                Login = "Ghosbi",
+                Name = "Pavel",
+                IsPremium = true
+            };
+
+            User user2 = new User()
+            {
+                Login = "Sannchi",
+                Name = "Sasha",
+                IsPremium = false
+            };
+
+            User user3 = new User()
+            {
+                Login = "Bircha",
+                Name = "Alex",
+                IsPremium = true
+            };
+
+            User user4 = new User()
+            {
+                Login = "Marki",
+                Name = "Maria",
+                IsPremium = false
+            };
+
+            List<User> listUser = new List<User>
+            {
+                user1,
+                user2,
+                user3,
+                user4
+            };
+
+            EligibleToShowAds eligibleToShowAds = ShowAds;
+
+            User.ToShowAds(listUser, eligibleToShowAds);
+
+
+
+        }
+        public static bool ShowAds(User user)
+        {
+            if (user.IsPremium == false)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+
+        static void Task12_1_2()
+        {
+            Console.WriteLine("Как вас зовут?");
+            string name = Console.ReadLine();
+            string greetings = "Привет, " + name;
+            Console.WriteLine(greetings);
+        }
+        static void Tastk12_1_3()
         {
             Console.WriteLine("Сколько элементов будет в массиве?");
 
@@ -16,13 +85,21 @@
             }
             Console.WriteLine("Все элементы записаны");
         }
-        
-        static void Task12_1_2()
+        static void Task12_1_4()
         {
-            Console.WriteLine("Как вас зовут?");
-            string name = Console.ReadLine();
-            string greetings = "Привет, " + name;
-            Console.WriteLine(greetings);
+            Console.WriteLine("Введите свой возраст");
+
+            var age = Int32.Parse(Console.ReadLine());
+            if (age > 13)
+            {
+                Console.WriteLine("Вы успешно зарегистрированы");
+            }
+            else
+            {
+                Console.WriteLine("Пользователи младше 14 лет не могут быть зарегистрированы");
+            }
         }
+
     }
+
 }
---
+++ Algorithm12/User.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Algorithm12.Program;
+
+namespace Algorithm12
+{
+    internal class User
+    {
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public bool IsPremium { get; set; }
+
+        public static void ToShowAds(List<User> listUser, EligibleToShowAds IsShowAdsEligible)
+        {
+            foreach (User user in listUser)
+            {
+                if (IsShowAdsEligible(user))
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+                    //Остановка на 1 с
+                    Thread.Sleep(1000);
+
+                    Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+                    Thread.Sleep(2000);
+
+                    Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу");
+                    Console.WriteLine("Реклама {0} показана", user.Name);
+                    //Остановка на 3с
+                    Thread.Sleep(3000);
+
+
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("У {0} есть премиум, ему реклама не показана", user.Name);
+                    //Остановка на 3с
+                    Thread.Sleep(3000);
+                }
+
+            }
+
+
+        }
+    }
+}
