41b20f5f75faed864670af0f361567430789d5d0
Pavel Konstantinov <Pavel.romon@gmail.com>
2023-08-02 22:55:24 +0500

    Переповторение модуля

---
+++ Work_With_FIles_Repetition/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace FirstApplication.ConsoleApp
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            // получим системные диски
+            DriveInfo[] drives = DriveInfo.GetDrives();
+
+            // Пробежимся по дискам и выведем их свойства
+            foreach (DriveInfo drive in drives)
+            {
+                Console.WriteLine($"Название: {drive.Name}");
+                Console.WriteLine($"Тип: {drive.DriveType}");
+                if (drive.IsReady)
+                {
+                    Console.WriteLine($"Объем: {drive.TotalSize}");
+                    Console.WriteLine($"Cвободно: {drive.TotalFreeSpace}");
+                    Console.WriteLine($"Метка: {drive.VolumeLabel}");
+                }
+            }
+        }
+    }
+}
